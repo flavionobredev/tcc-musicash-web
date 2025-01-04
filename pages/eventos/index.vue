826ecd1be128b154d5e4abba6493f1d890d26619
Flávio Nobre <flavionobredev@gmail.com>
2025-01-04 14:38:16 -0300

    feat: init modal

--- pages/eventos/index.vue
+++ pages/eventos/index.vue
@@ -4,6 +4,29 @@ onMounted(() => {
 });
 </script>
 
-<template>eventos!</template>
+<template>
+  <h2 class="text-3xl font-bold">Eventos</h2>
+
+  <section class="flex gap-4 mt-10">
+    <aside class="w-[248px]">
+      <UCard>
+        <EventsCreateModal>
+          <template #trigger="{ openModal }">
+            <UButton
+              @click="openModal"
+              icon="mdi:plus"
+              class="w-full font-bold justify-center"
+              >Novo Evento</UButton
+            >
+          </template>
+        </EventsCreateModal>
+      </UCard>
+    </aside>
+    <section>
+      <UCard> mini calendário</UCard>
+      <UCard> listagem de eventos por data</UCard>
+    </section>
+  </section>
+</template>
 
 <style scoped></style>
